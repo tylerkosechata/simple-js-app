35451020efce07981dbc60c0f1a58d396ef04d30
Tyler Kosechata <tykosechata@gmail.com>
2023-12-26 20:08:50 +0100

    Updating js\scrpit.js
    Adding Pokemon list with information

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
-alert('Hello world');
-
-let favoriteFood = 'Steak';
-document.write(favoriteFood);
+let pokemonList = [
+{
+    name: "Balbasuar", height: "0.7m ", weight: "6.9kg", type: ["grass","poison"]
+},
+{
+    name: "Charmander", height: "0.6m ", weight: "8.5kg", type: "fire"
+}, 
+{
+    name: "Squirtle", height: "0.5m ", weight: "9kg", type: "water"
+}
+];
