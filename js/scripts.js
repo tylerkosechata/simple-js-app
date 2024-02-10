63b9060ba0bde2cb630108ba74462551e88b9237
Tyler Kosechata <tykosechata@gmail.com>
2024-02-10 14:19:56 +0100

    Adding Repository and forEach
    Repository added for return. forEach added

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,34 @@
-// height is in m
-
-let pokemonList = [
-        {
-            name: 'Charizard',
-            height: 1.7,
-            type: ['fire', 'flying']
-        },
-        {
-            name: 'Squirtle',
-            height: .5,
-            type: ['water']
-        },
-        {
-            name: 'Bulbasaur',
-            height: .7,
-            type: ['grass', 'posion']
-        }
-    ];
-
-    for (let i=0; i < pokemonList; i++){
-        if (pokemonList[i].height >1.5){
-          console.log(pokemonList[i].name + " Wow that's big!!!");
-        }
+let pokemonRepository = (function () {
+    let pokemonList = [   {
+        name: 'Charizard',
+        height: 1.7,
+        type: ['fire', 'flying']
+    },
+    {
+        name: 'Squirtle',
+        height: .5,
+        type: ['water']
+    },
+    {
+        name: 'Bulbasaur',
+        height: .7,
+        type: ['grass', 'posion']
+    }];
+  
+    return {
+      add: function(pokemon) {
+        pokemonList.push(pokemon);
+      },
+      getAll: function() {
+        return pokemonList;
       }
+    };
+  })();
+  
+  console.log(pokemonRepository.getAll()); // []
+  pokemonRepository.add({ name: 'Pikachu' });
+  console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
+
+  pokemonList.forEach(function(name,height,type) {
+    console.log("name: " + name + " height: " + height + " type: " + type);
+  })
